c8066643ccee398ae7c22d1c4cb1c8a7d92e2d42
valeriy shtanko <vshtanko@itsmartflex.com>
2019-12-12 15:15:37 +0200

    Initial version of loyalty generated by JHipster-6.5.1

---
+++ src/test/java/com/icthh/xm/tmf/ms/loyalty/cucumber/stepdefs/StepDefs.java
@@ -0,0 +1,9 @@
+package com.icthh.xm.tmf.ms.loyalty.cucumber.stepdefs;
+
+import org.springframework.test.web.servlet.ResultActions;
+
+public abstract class StepDefs {
+
+    protected ResultActions actions;
+
+}
