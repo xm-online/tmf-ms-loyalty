ea8bd14588d673f56d29969ee3d221a9a30cfe2e
valeriy shtanko <vshtanko@itsmartflex.com>
2019-12-13 13:27:20 +0200

    Initialization. Step 2. Final.

--- src/main/java/com/icthh/xm/tmf/ms/loyalty/config/ApplicationProperties.java
+++ src/main/java/com/icthh/xm/tmf/ms/loyalty/config/ApplicationProperties.java
@@ -1,6 +1,13 @@
 package com.icthh.xm.tmf.ms.loyalty.config;
 
+import com.icthh.xm.commons.lep.TenantScriptStorage;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Properties specific to Loyalty.
@@ -8,6 +15,33 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  * Properties are configured in the {@code application.yml} file.
  * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
  */
+@Getter
+@Setter
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
+
+    private boolean kafkaEnabled;
+    private String kafkaSystemTopic;
+    private String kafkaSystemQueue;
+
+    private final Retry retry = new Retry();
+    private final Lep lep = new Lep();
+    private List<String> tenantIgnoredPathList = Collections.emptyList();
+
+    private boolean timelinesEnabled;
+
+    @Getter
+    @Setter
+    public static class Lep {
+        private TenantScriptStorage tenantScriptStorage;
+        private String lepResourcePathPattern;
+    }
+
+    @Getter
+    @Setter
+    private static class Retry {
+        private int maxAttempts;
+        private long delay;
+        private int multiplier;
+    }
 }
